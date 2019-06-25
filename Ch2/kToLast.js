7dda8999e654cb5773eebb969892cbfb748c0a47
Dan Gilbert <daniel.gilbert0051@gmail.com>
2019-06-25 13:27:45 -0400

    solved: kthToLast

--- Ch2/kToLast.js
+++ Ch2/kToLast.js
@@ -1,5 +1,3 @@
-console.log('________________________');
-
 class Node {
   constructor(val, next = null) {
     this.val = val;
@@ -46,13 +44,3 @@ class SinglyLinkedList {
     return trailNode;
   }
 }
-
-const list = new SinglyLinkedList();
-list.push(1);
-list.push(2);
-list.push(3);
-list.push(4);
-list.push(5);
-list.push(6);
-
-console.log(list.kthToLast(6));
